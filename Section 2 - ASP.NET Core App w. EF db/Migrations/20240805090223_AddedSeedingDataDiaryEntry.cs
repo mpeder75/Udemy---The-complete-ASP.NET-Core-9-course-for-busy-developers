c5dc5e6c424a5b5bae1c0272b928f21a4fc4b6a2
Michael <mpeder75@gmail.com>
2024-09-03 16:43:03 +0200

    Add project files.

---
+++ Section 2 - ASP.NET Core App w. EF db/Migrations/20240805090223_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Diary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2024, 8, 5, 11, 2, 22, 493, DateTimeKind.Local).AddTicks(5525), "Went hiking" },
+                    { 2, "Went shopping with Joe!", new DateTime(2024, 8, 5, 11, 2, 22, 493, DateTimeKind.Local).AddTicks(5921), "Went shopping" },
+                    { 3, "Went diving with Joe!", new DateTime(2024, 8, 5, 11, 2, 22, 493, DateTimeKind.Local).AddTicks(5925), "Went diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ Section 2 - ASP.NET Core App w. EF db/Models/DiaryEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Diary.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Title")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters")]
+        public string Title { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "Please enter Content")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Content must be between 3 and 100 characters")]
+        public string Content { get; set; } = String.Empty;
+
+        [Required]        
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
