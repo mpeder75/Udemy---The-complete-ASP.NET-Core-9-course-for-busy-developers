45323ae2dd46c3b852409cda2bd117e4bcd1fef8
Michael <mpeder75@gmail.com>
2024-09-03 20:43:51 +0200

    minor update

--- WebDiaryAPI/Controllers/WeatherForecastController.cs
+++ WebDiaryAPI/Controllers/WeatherForecastController.cs
@@ -2,14 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebDiaryAPI.Controllers
 {
+    // [ApiController] attribute is used to indicate that the class is a controller
     [ApiController]
+    // [Route] attribute is used to specify the route template for the controller
     [Route("[controller]")]
+
+    // WeatherForecastController class inherits from ControllerBase class
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private static readonly string[] Summaries = new[] {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,7 +19,9 @@ namespace WebDiaryAPI.Controllers
             _logger = logger;
         }
 
+        // [HttpGet] attributten definere en HTTP GET metode med navnet GetWeatherForecast
         [HttpGet(Name = "GetWeatherForecast")]
+        // Get metode der returnere en IEnumerable af WeatherForecast objekter
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- WebDiaryAPI/Program.cs
+++ WebDiaryAPI/Program.cs
@@ -1,23 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using WebDiaryAPI.Data;
+
+// WebApplication is a class that provides a way to configure and run a web application.
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// tilføjer connectionString fra appsettings.json til IoC containeren
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Configure the HTTP request pipeline.
+var app = builder.Build();
+
+// If the environment is development, the OpenAPI specification is generated and Swagger UI is enabled.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+// Run the application.
 app.Run();
+
